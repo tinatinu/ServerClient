b045d223f89809e00093bf3bd1e5f56955015aa6
Thazniya <thasniyakader@gmail.com>
2018-03-20 14:13:57 +0530

    client second part

--- GossipClient.java
+++ GossipClient.java
@@ -9,3 +9,13 @@ public class GossipClient
                                // reading from keyboard (keyRead object)
      BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
                               // sending to client (pwrite object)
+                              OutputStream ostream = sock.getOutputStream(); 
+     PrintWriter pwrite = new PrintWriter(ostream, true);
+ 
+                              // receiving from server ( receiveRead  object)
+     InputStream istream = sock.getInputStream();
+     BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+ 
+     System.out.println("Start the chitchat, type and press Enter key");
+ 
+     String receiveMessage, sendMessage;   
