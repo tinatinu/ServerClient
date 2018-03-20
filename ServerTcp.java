d49efca7d524abb07d20fc1a3d59e2e755598294
Tina <tinachemmannur10@gmail.com>
2018-03-20 14:20:36 +0530

    Server first commit

---
+++ ServerTcp.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.net.*;
+public class ServerTcp
+{
+  public static void main(String[] args) throws Exception
+  {
+      ServerSocket sersock = new ServerSocket(3000);
+      System.out.println("Server  ready for chatting");
+      Socket sock = sersock.accept( );   
