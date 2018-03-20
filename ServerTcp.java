bdb34c14faea6c89b3442d79ca17786d54721b27
Tina <tinachemmannur10@gmail.com>
2018-03-20 14:21:38 +0530

    Server second commit

--- ServerTcp.java
+++ ServerTcp.java
@@ -6,4 +6,14 @@ public class ServerTcp
   {
       ServerSocket sersock = new ServerSocket(3000);
       System.out.println("Server  ready for chatting");
-      Socket sock = sersock.accept( );   
+      Socket sock = sersock.accept( );  
+      BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+	                     
+      OutputStream ostream = sock.getOutputStream(); 
+      PrintWriter pwrite = new PrintWriter(ostream, true);
+ 
+                              
+      InputStream istream = sock.getInputStream();
+      BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+ 
+      String receiveMessage, sendMessage;      
