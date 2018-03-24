4fec71602b563215a6c01e57116d9c95a1f0147a
Tina <tinachemmannur10@gmail.com>
2018-03-24 11:02:18 +0530

    server committing files

--- ServerTcp.java
+++ ServerTcp.java
@@ -4,7 +4,13 @@ class MyServer
 {  
     public static void main(String args[])throws Exception
     {  
-        ServerSocket ss=new ServerSocket(3333);  
+        ServerSocket ss=new ServerSocket(3333);
+        Socket s=ss.accept();  
+        DataInputStream din=new DataInputStream(s.getInputStream());  
+        DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+      
+        String str="",str2="";    
 
 
 
