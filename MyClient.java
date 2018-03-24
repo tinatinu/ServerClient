cef9f91709e80524228c19bd412699afafded012
Thazniya <thasniyakader@gmail.com>
2018-03-24 10:59:22 +0530

    client file modified

--- MyClient.java
+++ MyClient.java
@@ -4,3 +4,5 @@ import java.net.*;
     public static void main(String args[])throws Exception{ 
     Socket s=new Socket("localhost",3333);  
     DataInputStream din=new DataInputStream(s.getInputStream()); 
+    DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
