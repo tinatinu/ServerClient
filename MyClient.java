d4c976e194d90b7c8b28e18301c1faa583749573
Thazniya <thasniyakader@gmail.com>
2018-03-24 10:59:05 +0530

    client file modified

--- MyClient.java
+++ MyClient.java
@@ -2,3 +2,5 @@ import java.net.*;
     import java.io.*; 
     class MyClient{  
     public static void main(String args[])throws Exception{ 
+    Socket s=new Socket("localhost",3333);  
+    DataInputStream din=new DataInputStream(s.getInputStream()); 
